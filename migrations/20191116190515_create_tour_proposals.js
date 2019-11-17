ee7078a1aee103bed00ac609ee332f9bd42d0a93
jjh <j.h.jeong@outlook.com>
2019-11-17 17:44:56 +0900

    Migration typo fixes

--- migrations/20191116190515_create_tour_proposals.js
+++ migrations/20191116190515_create_tour_proposals.js
@@ -10,8 +10,8 @@ exports.up = function(knex) {
     // table.integer("prop_customer_id").reference("customer.customer_id");
     table
       .integer("prop_tour_request_id")
-      .reference("tour_request.tour_request_id");
-    table.integer("prop_guide_id").reference("guide.guide_id");
+      .references("tour_request.tour_request_id");
+    table.integer("prop_guide_id").references("guide.guide_id");
   });
 };
 
